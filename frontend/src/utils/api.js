dededa46f9f16493b64b1fe1319b04e8aa130087
Giang Dao <daotruonggiang91@gmail.com>
2023-06-15 00:34:48 +0700

    add support for upload images

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,6 +2,7 @@ export function makeRequest(method, url, data) {
   return new Promise(function (resolve, reject) {
     let xhr = new XMLHttpRequest();
     xhr.open(method, url);
+    xhr.setRequestHeader('Content-Type', 'application/json; charset=utf-8');
     xhr.onload = function () {
       if (this.status >= 200 && this.status < 300) {
         resolve(xhr.response);
